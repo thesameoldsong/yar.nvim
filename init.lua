53ffb49213898b0c5cb4a41009cc1659ddc1516b
Anatoly <anthony.z@comradeweb.com>
2025-12-23 23:38:15 +0300

    added support URL encoded space in md links

--- init.lua
+++ init.lua
@@ -107,6 +107,30 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
+local function smart_markdown_follow()
+  local line = vim.api.nvim_get_current_line()
+
+  local start_pos = line:find('%(', 1)
+  local end_pos = line:find('%)', start_pos)
+
+  if start_pos and end_pos then
+    local path = line:sub(start_pos + 1, end_pos - 1)
+
+    path = path:gsub('%%20', ' ')
+
+    vim.cmd('edit ' .. vim.fn.fnameescape(path))
+  else
+    vim.api.nvim_feedkeys('gf', 'n', false)
+  end
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.keymap.set('n', 'gf', smart_markdown_follow, { buffer = true, desc = 'Follow link with space decoding' })
+  end,
+})
+
 -- [[ Configure and install plugins ]]
 --  To update plugins you can run
 --    :Lazy update
